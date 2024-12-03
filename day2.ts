3f18930df52e6d5a478e9d70e782710b3af146ed
ninja03 <69106571+ninja03@users.noreply.github.com>
2024-12-03 19:59:52 +0900

    Create day2.ts

---
+++ day2.ts
@@ -0,0 +1,37 @@
+import { TextLineStream } from '@std/streams'
+
+let safe = 0; // 安全の数
+let prev = 0; // 前の数値
+let sign = 0; // 増加か減少か
+
+const stream = Deno.stdin.readable.pipeThrough(new TextDecoderStream()).pipeThrough(new TextLineStream());
+LOOP: for await (const line of stream) {
+  if (line == "") continue;
+  const cols = line.split(" ");
+
+  for (let i = 0; i < cols.length; i++) {
+    const num = Number(cols[i]);
+    if (i === 1) {
+      // 2番目の数字で増加か減少の行か決まる
+      sign = num > prev ? 1 : -1;
+    }
+    if (i >= 1) {
+      // 変化しないなら危険
+      if (num === prev) continue LOOP;
+      // 変化幅が3を超えるので危険
+      if (Math.abs(num - prev) > 3) continue LOOP;
+    }
+    if (i >= 2) {
+      // 変化しないなら危険
+      if (num === prev) continue LOOP;
+      // 増加の行なのに減少なら危険
+      if (sign === 1 && num < prev) continue LOOP;
+      // 減少の行なのに増加なら危険
+      if (sign === -1 && num > prev) continue LOOP;
+    }
+    prev = num;
+  }
+  safe++;
+}
+
+console.log(safe);
